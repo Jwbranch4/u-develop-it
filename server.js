15dcc54e70b0fe4355206a1eb593b536ec3b07b0
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2021-10-21 14:40:49 -0500

    created first db.query

--- server.js
+++ server.js
@@ -4,9 +4,31 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Import mysql
+const mysql = require("mysql2");
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "Iceman2010",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+// query the database
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
